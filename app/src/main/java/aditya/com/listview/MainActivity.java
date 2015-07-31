cecab1c57d1ef8fda2f1fb9a7b03cd1894a428f6
Aditya Agarwal <skymanaditya1@gmail.com>
2015-07-31 14:45:53 +0530

    Implemented the listview in android, with the option of removing an item from the list

--- app/src/main/java/aditya/com/listview/MainActivity.java
+++ app/src/main/java/aditya/com/listview/MainActivity.java
@@ -1,17 +1,72 @@
 package aditya.com.listview;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    String[] items = {"ListView", "Implementation", "Android Studio"};
+    ListView lv;
+    List<String> arr;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lv = (ListView) findViewById(R.id.listView1);
+        arr = new ArrayList<String>(Arrays.asList(items));
+        adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,
+                arr);
+        lv.setAdapter(adapter);
+
+        registerClickCallBack();
+    }
+
+    // Method to handle the clicks on the list item
+    private void registerClickCallBack() {
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view, final int position, long id) {
+                // position is the index of the list item that was pressed
+                String txt = ((TextView) view).getText().toString();
+                String msg = "You pressed position #"+position+" which is "+txt;
+                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(MainActivity.this);
+                dialogBuilder.setTitle("Do you want to remove the item from ths list?");
+                dialogBuilder.setMessage(msg);
+                dialogBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        arr.remove(position);
+                        lv.invalidateViews();
+                        // notifyAll();
+                    }
+                }).setNegativeButton("Ignore", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "Ignored", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                AlertDialog dialog = dialogBuilder.create();
+                dialog.show();
+            }
+        });
     }
 
 
